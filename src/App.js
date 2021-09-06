9205cc035d814759e695123f40b8fc18a077e202
toddcford <toddcford@gmail.com>
2021-09-05 22:11:00 -0500

    imported HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { BrowserRouter,Route, Link, Switch } from 'react-router-dom';
+import { HashRouter,BrowserRouter,Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
 import About from './pages/About/About'
