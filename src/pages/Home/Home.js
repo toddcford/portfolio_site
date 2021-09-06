364656f9149c1054fc3ba9a6a16a616e3970606f
toddcford <toddcford@gmail.com>
2021-09-05 21:32:21 -0500

    update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home-wrapper">
       
-      <img id="home-photo" src={process.env.PUBLIC_URL + "Images/me.jpg"} alt={"toddford"}/>
+      <img id="home-photo" src={process.env.PUBLIC_URL + "/Images/me.jpg"} alt={"toddford"}/>
       
       <h1 className="home-title"> Todd Ford </h1>
       <h4 className="home-subtitle">Software Developer </h4>
